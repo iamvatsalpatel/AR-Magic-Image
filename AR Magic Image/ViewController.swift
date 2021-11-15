8c425446d84ee6c8a372663ca44fb8f786905b3e
Vatsal Patel <vatsalcodes@gmail.com>
2021-11-15 22:36:26 +0530

    Files

---
+++ AR Magic Image/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  AR Magic Image
+//
+//  Created by Vatsal Patel on 15/11/21.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "Image", bundle: Bundle.main) {
+            
+            configuration.trackingImages = trackedImages
+            
+            configuration.maximumNumberOfTrackedImages = 1
+        }
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            let videofile = "Video.mov"
+            
+            let videoNode = SKVideoNode(fileNamed: videofile)
+            
+            
+            videoNode.play()
+            
+            let videoScene = SKScene(size: CGSize(width: 960, height: 540))
+            
+            
+            videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+            
+            videoNode.yScale = -1.0
+            
+            videoScene.addChild(videoNode)
+            
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = videoScene
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+            
+        }
+        
+        return node
+        
+    }
+    
+}
